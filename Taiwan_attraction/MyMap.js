dac9d5f674afc7c91c28278a7a3692a843692ecf
sonia1013 <71934074+sonia1013@users.noreply.github.com>
2021-01-17 17:46:44 +0800

    Add files via upload
    init 0117

---
+++ Taiwan_attraction/MyMap.js
@@ -0,0 +1,32 @@
+//載入臺灣各地區資料
+function GetTaiwanData() {
+  let;
+}
+
+//動態生成+顯示圖標
+function ShowMarkersInMap() {
+  // mapArray.forEach(index, => {
+
+  // });
+  var attractionsLatLng = new google.map.Map(tourSights.px, tourSights.py);
+  let newLocation = new google.map.Map(document.getElementById("map"), {});
+}
+
+/*
+        var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+        var mapOptions = {
+          zoom: 4,
+          center: myLatlng
+        }
+          */
+
+//let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 25.0415956, lng: 121.5341098 },
+//     zoom: 16,
+//   });
+// }
+
+//To add the marker to the map, call setMap();
+//marker.setMap(map);
